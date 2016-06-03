cf2513c86f09dbdb7b139921119760b717aeb41f
Joshua Robusto <jrobusto@42six.com>
2016-06-03 14:36:11 -0700

    Added ability to change matrix size

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -84,6 +84,56 @@ function submitMatrix(){
   var augmented = retrieveMatrix();
   console.log(augmented);
 }
+
+function addRow(){
+  var coefficientsElem = document.querySelector('#coefficients');
+  var rows = coefficientsElem.querySelectorAll('paper-listbox');
+  var newRow = rows[0].cloneNode(true);
+  coefficientsElem.appendChild(newRow);
+
+  var solutionVector = document.querySelector('#solution-vector>paper-listbox');
+  var values = solutionVector.querySelectorAll('paper-textarea');
+  var newValue = values[0].cloneNode(true);
+  solutionVector.appendChild(newValue);
+}
+
+function removeRow(){
+  var coefficientsElem = document.querySelector('#coefficients');
+  var rows = coefficientsElem.querySelectorAll('paper-listbox');
+  if (rows.length > 1){
+    var lastRow = rows[rows.length - 1];
+    lastRow.remove();
+
+    var solutionVector = document.querySelector('#solution-vector>paper-listbox');
+    var values = solutionVector.querySelectorAll('paper-textarea');
+    var lastValue = values[values.length - 1];
+    lastValue.remove();
+  }
+}
+
+function addColumn(){
+  var coefficientsElem = document.querySelector('#coefficients');
+  var rows = coefficientsElem.querySelectorAll('paper-listbox');
+  for (var i = 0; i < rows.length; i++){
+    var newCell = rows[i].querySelector('paper-textarea').cloneNode(true);
+    rows[i].appendChild(newCell);
+  }
+}
+
+function removeColumn(){
+  var coefficientsElem = document.querySelector('#coefficients');
+  var rows = coefficientsElem.querySelectorAll('paper-listbox');
+  for (var i = 0; i < rows.length; i++){
+    var cells = rows[i].querySelectorAll('paper-textarea');
+    if (cells.length > 1){
+      var lastCell = cells[cells.length - 1];
+      lastCell.remove();
+    }else{
+      break;
+    }
+  }
+}
+
 function retrieveMatrix(){
   var augmented = {};
   var calc = document.querySelector('#calc');
